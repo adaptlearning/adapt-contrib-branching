a4e009c98603b9e86a72818987cc0279235dbd19
Chris Steele <chris.steele@kineo.com>
2025-01-28 12:44:29 +0000

    Migration corrections

--- migrations/v1.js
+++ migrations/v1.js
@@ -1,15 +1,15 @@
 import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 
-describe('Branching - v1.0.0 to v1.0.3', async () => {
+describe('Branching - v1.0.1 to v1.0.3', async () => {
 
-  // https://github.com/adaptlearning/adapt-contrib-branching/compare/v1.0.0..v1.0.3
+  // https://github.com/adaptlearning/adapt-contrib-branching/compare/v1.0.1..v1.0.3
 
   // note https://github.com/adaptlearning/adapt-contrib-branching/compare/v1.0.2..v1.0.3
   // _start was misplaced, but this only occurred for 30 mins so it will be ignored
 
   let configuredArticles;
 
-  whereFromPlugin('Branching - from v1.0.0', { name: 'adapt-contrib-branching', version: '<1.0.3' });
+  whereFromPlugin('Branching - from v1.0.1', { name: 'adapt-contrib-branching', version: '<1.0.3' });
 
   whereContent('Branching - where branching configured on articles', async (content) => {
     configuredArticles = content.filter(({ _type, _branching }) => _type === 'article' && _branching);
@@ -55,9 +55,9 @@ describe('Branching - v1.0.3 to v1.1.0', async () => {
   updatePlugin('Branching - update to v1.1.0', { name: 'adapt-contrib-branching', version: '1.1.0', framework: '">=5.19.1' });
 });
 
-describe('Branching - v1.1.0 to v1.2.0', async () => {
+describe('Branching - v1.1.2 to v1.2.0', async () => {
 
-  // https://github.com/adaptlearning/adapt-contrib-branching/compare/v1.1.0..v1.2.0
+  // https://github.com/adaptlearning/adapt-contrib-branching/compare/v1.1.2..v1.2.0
 
   let configuredBlocks;
   const attemptBands = [{
@@ -67,7 +67,7 @@ describe('Branching - v1.1.0 to v1.2.0', async () => {
     _partlyCorrect: ''
   }];
 
-  whereFromPlugin('Branching - from v1.1.0', { name: 'adapt-contrib-branching', version: '<1.2.0' });
+  whereFromPlugin('Branching - from v1.1.2', { name: 'adapt-contrib-branching', version: '<1.2.0' });
 
   whereContent('Branching - where branching configured on blocks', async (content) => {
     configuredBlocks = content.filter(({ _type, _branching }) => _type === 'block' && _branching);
@@ -107,13 +107,13 @@ describe('Branching - v1.1.0 to v1.2.0', async () => {
   updatePlugin('Branching - update to v1.2.0', { name: 'adapt-contrib-branching', version: '1.2.0', framework: '">=5.19.1' });
 });
 
-describe('Branching - v1.2.0 to v1.3.3', async () => {
+describe('Branching - v1.2.3 to v1.3.0', async () => {
 
-  // https://github.com/adaptlearning/adapt-contrib-branching/compare/v1.2.0..v1.3.3
+  // https://github.com/adaptlearning/adapt-contrib-branching/compare/v1.2.3..v1.3.0
 
   let configuredBlocks;
 
-  whereFromPlugin('Branching - from v1.2.0', { name: 'adapt-contrib-branching', version: '<1.2.0' });
+  whereFromPlugin('Branching - from v1.2.3', { name: 'adapt-contrib-branching', version: '<1.3.0' });
 
   whereContent('Branching - where branching configured on blocks', async (content) => {
     configuredBlocks = content.filter(({ _type, _branching }) => _type === 'block' && _branching);
@@ -130,5 +130,5 @@ describe('Branching - v1.2.0 to v1.3.3', async () => {
     return !isInvalid;
   });
 
-  updatePlugin('Branching - update to v1.3.3', { name: 'adapt-contrib-branching', version: '1.3.3', framework: '">=5.19.1' });
+  updatePlugin('Branching - update to v1.3.0', { name: 'adapt-contrib-branching', version: '1.3.0', framework: '">=5.19.1' });
 });
